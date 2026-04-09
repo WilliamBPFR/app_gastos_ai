f49c33842c27ccd1b12a1d99ec594888fd83b889
WilliamBPFR <williamchawillferferreira@gmail.com>
2026-04-09 01:09:09 -0400

    Mejora mensaje de health endpoint

--- routers/health_router.py
+++ routers/health_router.py
@@ -4,4 +4,4 @@ router = APIRouter(tags=["health"])
 
 @router.get("/health")
 def health():
-    return {"status": "ok"}
+    return {"status": "ok", "message": "API is healthy", "version": "1.0.1", "Descripcion": "Agregado verificacion de usuario a endpoint de start oauth, para evitar errores al iniciar el proceso de autorizacion con un usuario no registrado en la base de datos."}
